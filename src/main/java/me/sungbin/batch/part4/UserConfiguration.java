8d6616d3b8d8e180f1bb3a70c9cb735d796857ea
SungbinYang <ysb05222@naver.com>
2021-09-20 01:27:17 +0900

    JobExecutionListener로 대상 회원 데이터 로그와 실행시간 측정

--- src/main/java/me/sungbin/batch/part4/UserConfiguration.java
+++ src/main/java/me/sungbin/batch/part4/UserConfiguration.java
@@ -36,6 +36,7 @@ public class UserConfiguration {
                 .incrementer(new RunIdIncrementer())
                 .start(this.saveUserStep())
                 .next(this.userLevelUpStep())
+                .listener(new LevelUpJobExecutionListener(userRepository))
                 .build();
     }
 
